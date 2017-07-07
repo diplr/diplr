30c9537e63038d9756f0c3f4290a5fedd9b66b8d
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-07 15:32:24 +0200

    moving stuff around

--- scripts/copydir.js
+++ scripts/copydir.js
@@ -1,4 +1,38 @@
-var copydir = require('copy-dir');
- 
-copydir.sync('app/theme', 'dist/');
-copydir.sync('app/images', 'dist/images');
+var copy = require('copy'); 
+
+//copy template
+copy('app/theme/**/**.js', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.css', 'dist', function(err, files){
+  if(err) throw err;
+});
+
+//theme images
+copy('app/theme/**/**.gif', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.png', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.jpg', 'dist', function(err, files){
+  if(err) throw err;
+});
+
+//theme fonts
+copy('app/theme/**/**.woff', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.eot', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.ttf', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.woff2', 'dist', function(err, files){
+  if(err) throw err;
+});
+copy('app/theme/**/**.svg', 'dist', function(err, files){
+  if(err) throw err;
+});
+
