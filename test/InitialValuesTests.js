98b5f0fd548156e7c4f7bbd0e435dc4071f84469
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-18 19:19:50 +0200

    writing tests

---
+++ test/InitialValuesTests.js
@@ -0,0 +1,37 @@
+var CrowdsaleToken = artifacts.require("./token/CrowdsaleToken.sol");
+
+contract('CrowdsaleToken', function (accounts) {
+  it("it should have the name DIPLR", function () {
+    return CrowdsaleToken.deployed().then(function (instance) {
+      return instance.name.call();
+    }).then(function (_name) {
+      assert.equal(_name.valueOf(), "diplr", "DIPLR was not the name of the token");
+    });
+  });
+
+  it("it should have the symbol DPL", function () {
+    return CrowdsaleToken.deployed().then(function (instance) {
+      return instance.symbol.call();
+    }).then(function (_name) {
+      assert.equal(_name.valueOf(), "DPL", "DPL was not the symbol of the token");
+    });
+  });
+
+  it("it should have 18 decimals", function () {
+    return CrowdsaleToken.deployed().then(function (instance) {
+      return instance.symbol.call();
+    }).then(function (_name) {
+      assert.equal(_name.valueOf(), "18", "18 was not the amount of decimals");
+    });
+  });
+
+  it("it should an empty initial supply", function () {
+    return CrowdsaleToken.deployed().then(function (instance) {
+      return instance.initialSupply.call();
+    }).then(function (_supply) {
+      assert.equal(_supply.valueOf(), "0", "initial supply was not empty");
+    });
+  });
+
+
+});
