e027a2ede764a6dba1c6785fba60f5e2ea3c9915
Quinten De Swaef <quintendeswaef@gmail.com>
2017-07-18 20:38:29 +0200

    fix test

--- test/InitialValuesTests.js
+++ test/InitialValuesTests.js
@@ -19,15 +19,15 @@ contract('CrowdsaleToken', function (accounts) {
 
   it("it should have 18 decimals", function () {
     return CrowdsaleToken.deployed().then(function (instance) {
-      return instance.symbol.call();
-    }).then(function (_name) {
-      assert.equal(_name.valueOf(), "18", "18 was not the amount of decimals");
+      return instance.decimals.call();
+    }).then(function (_decimals) {
+      assert.equal(_decimals.valueOf(), "18", "18 was not the amount of decimals");
     });
   });
 
   it("it should an empty initial supply", function () {
     return CrowdsaleToken.deployed().then(function (instance) {
-      return instance.initialSupply.call();
+      return instance.totalSupply.call();
     }).then(function (_supply) {
       assert.equal(_supply.valueOf(), "0", "initial supply was not empty");
     });
